f41216cd030318519354355908b27d464e1ef48c
Abh1nayg <ghimireabhinaya80@gmail.com>
2025-03-31 04:24:43 +0530

    Value changed again

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function calculateLove() {
       }
       
       // Long name nerf (chance-based)
-      if (letterCount >= longNameThreshold && Math.random() < 0.7) { // 70% chance
+      if (letterCount >= longNameThreshold && Math.random() < 0.95) { // 70% chance
         let nerfAmount = Math.floor(Math.random() * 15) + 15; // -5 to -20 points
         modifier -= nerfAmount;
         console.log("Long name nerf: -" + nerfAmount);
